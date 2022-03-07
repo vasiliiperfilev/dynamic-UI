04192ab8edda3777cec85425b99cf41343c6036a
Vasilii Perfilev <vasily.perfilyev@gmail.com>
2022-03-07 10:52:05 -0800

    Refactor to make a module

--- src/index.js
+++ src/index.js
@@ -13,5 +13,56 @@ function triggerMenu() {
   }
 }
 
-document.querySelector('.trigger').addEventListener('click', triggerMenu);
-document.querySelector('.mobile-trigger').addEventListener('click', triggerMenu);
+document.querySelector('.mobile.trigger').addEventListener('click', triggerMenu);
+
+function createDOMelement(tag, textContent, ...classList) {
+  const element = document.createElement(tag);
+  if (classList[0]) element.classList.add(classList);
+  if (textContent) element.textContent = textContent;
+  return element;
+}
+
+function changeElementColors(colorOptions, ...elementList) {
+  elementList.forEach((element) => {
+    Object.entries(colorOptions).forEach(([key, value]) => {
+      element.style[key] = value;
+    });
+  });
+}
+
+function addLiToUl(ul, liNamesList, colorOptions) {
+  liNamesList.forEach((name) => {
+    const li = createDOMelement('li', name, null);
+    changeElementColors(colorOptions, li);
+    ul.append(li);
+  });
+}
+
+function createMenu(menuName, menuNamesList, colorOptions) {
+  const trigger = createDOMelement('div', null, 'trigger');
+  const menu = createDOMelement('ul', null, 'menu');
+  addLiToUl(menu, menuNamesList, colorOptions);
+  changeElementColors(colorOptions, trigger, menu);
+  const span = createDOMelement('span', menuName, null);
+  trigger.append(span, menu);
+  trigger.addEventListener('click', triggerMenu);
+  return trigger;
+}
+
+function addMenuIcons(symbolsList){
+  
+}
+
+function createMobileMenu(menuNamesList, colorOptions) {
+  const menu = createMenu('', menuNamesList, colorOptions);
+  menu.classList.add('mobile');
+  return menu;
+}
+
+const colorOptions = {
+  color: 'rgb(235, 255, 226)',
+  backgroundColor: 'rgb(0, 128, 85)',
+};
+const menuNames = ['Option 1', 'Long option 2', 'ShOpt3'];
+const menu = createMenu('MenuOne', menuNames, colorOptions);
+document.querySelector('header').append(menu);
