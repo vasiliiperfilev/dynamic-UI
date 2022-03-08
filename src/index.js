84c67e5814d5218c1cab3540f6093c8c7be33293
Vasilii Perfilev <vasily.perfilyev@gmail.com>
2022-03-08 10:49:45 -0800

    Add change style feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable quote-props */
 import './styles/normalize.css';
 import './styles/styles.css';
 
@@ -13,8 +14,6 @@ function triggerMenu() {
   }
 }
 
-document.querySelector('.mobile.trigger').addEventListener('click', triggerMenu);
-
 function createDOMelement(tag, textContent, ...classList) {
   const element = document.createElement(tag);
   if (classList[0]) element.classList.add(classList);
@@ -22,47 +21,63 @@ function createDOMelement(tag, textContent, ...classList) {
   return element;
 }
 
-function changeElementColors(colorOptions, ...elementList) {
-  elementList.forEach((element) => {
-    Object.entries(colorOptions).forEach(([key, value]) => {
-      element.style[key] = value;
-    });
+function changeElementsStyle(styles) {
+  const style = document.createElement('style');
+  Object.entries(styles).forEach(([selector, properties]) => {
+    const stringProperties = JSON.stringify(properties);
+    const unquoted = stringProperties.replace(/,?"([^"]+)":"([^"]+),?"/g, '$1:$2;');
+    style.innerHTML += `${selector} ${unquoted}`;
   });
+  document.querySelector('head').appendChild(style);
 }
 
-function addLiToUl(ul, liNamesList, colorOptions) {
+function addLiToUl(ul, liNamesList) {
   liNamesList.forEach((name) => {
     const li = createDOMelement('li', name, null);
-    changeElementColors(colorOptions, li);
     ul.append(li);
   });
 }
 
-function createMenu(menuName, menuNamesList, colorOptions) {
+function createMenu(menuName, menuNamesList, styles) {
   const trigger = createDOMelement('div', null, 'trigger');
   const menu = createDOMelement('ul', null, 'menu');
-  addLiToUl(menu, menuNamesList, colorOptions);
-  changeElementColors(colorOptions, trigger, menu);
+  addLiToUl(menu, menuNamesList, styles);
+  changeElementsStyle(styles);
   const span = createDOMelement('span', menuName, null);
   trigger.append(span, menu);
   trigger.addEventListener('click', triggerMenu);
   return trigger;
 }
 
-function addMenuIcons(symbolsList){
-  
+function addMenuIcons(menu, symbolsList) {
+  const ul = menu.querySelector('ul');
+  ul.childNodes.forEach((child, index) => {
+    const span = createDOMelement('span', symbolsList[index], null);
+    child.append(span);
+  });
 }
 
-function createMobileMenu(menuNamesList, colorOptions) {
-  const menu = createMenu('', menuNamesList, colorOptions);
+function createMobileMenu(menuNamesList, styles, symbolsList) {
+  const menu = createMenu('', menuNamesList, styles);
   menu.classList.add('mobile');
+  addMenuIcons(menu, symbolsList);
   return menu;
 }
 
-const colorOptions = {
-  color: 'rgb(235, 255, 226)',
-  backgroundColor: 'rgb(0, 128, 85)',
+const styles = {
+  '.trigger': {
+    'background-color': 'rgb(0, 128, 85)',
+    'color': 'rgb(235, 255, 226)',
+  },
 };
 const menuNames = ['Option 1', 'Long option 2', 'ShOpt3'];
-const menu = createMenu('MenuOne', menuNames, colorOptions);
+const menu = createMenu('MenuOne', menuNames, styles);
 document.querySelector('header').append(menu);
+const mobileStyles = {
+  '.mobile.trigger': {
+    'background-color': 'rgb(0, 128, 85)',
+    'color': 'rgb(235, 255, 226)',
+  },
+};
+const mobileMenu = createMobileMenu(menuNames, mobileStyles, ['+', '!', 'x']);
+document.querySelector('body').append(mobileMenu);
